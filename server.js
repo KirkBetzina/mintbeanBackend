f92a830e5412781fdc8c35efc1f8fe492a4db019
kirk <kirk.betzina@gmail.com>
2021-07-30 22:05:18 -0400

    made controller and model files, and server.

--- server.js
+++ server.js
@@ -1,7 +1,36 @@
 require('dotenv').config();
 const mongoose = require('./db/connection');
 const moorgan = require('morgan');
-const cors = require('cors')
+const cors = require('cors');
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT
+
+///////////////////////////
+// MIDDLEWARE 
+///////////////////////////
+const logger = require('morgan')
+app.use(logger('dev'))
+app.use(exprees.urlencoded({extended:false}))
+app.use(express.json())
+
+/////////////////////////////
+// Add our Default rte
+/////////////////////////////
+
+app.get('/' , (req, res) => {
+    res.json({
+        status: 200,
+        msg: 'hitting the default rte'
+    });
+})
+
+/////////////////////////////////
+// Import Your Controller
+////////////////////////////////
+const UserController = require('./users/controllers/user');
+app.use('/user', UserController)
+
+app.listen(PORT, () => {
+    console.log(`listening in on port ${PORT}`)
+})
---
+++ users/controllers/user.js
@@ -0,0 +1,50 @@
+const {Router} = require('express');
+const express = require('express')
+
+const router = express.Router()
+const user = require('../model/user')
+
+router.get('/', (req, res) => {
+    res.json({
+        status:200,
+        data: user
+    })
+})
+
+router.get('/:id', (req, res) => {
+    res.json({
+        status:200,
+        data: user[req.params.id]
+    })
+})
+
+router.post('/', (req, req) => {
+    console.log('POST - req.body', req.body)
+    user.push(req.body)
+    res.json({        
+        status: 200,
+        route: "YOURE HITTING ME IN THE POST ROUTE",
+        data: req.body
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    user.splice(req.params.id, 1);
+
+    res.json({
+        status:200,
+        route: "EJECTO DATA'O CUZ"
+    })
+})
+
+router.put('/:id', (req, res) =>{
+    console.log('PUT - req.params', req.params)
+    console.log("PUT - req.body", req.body)
+    user[req.params.id] = req.body
+    res.json({
+        status:200,
+        route: "YOURE HITTING THE PUT ROUTE"
+    })
+})
+
+module.exports = router
